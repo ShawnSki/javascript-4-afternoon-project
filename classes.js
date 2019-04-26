a36ce26248a4a414665ac0af4d0250ee84587682
shawn's macbook <shawnscivally@shawn-ski.local>
2019-04-26 17:51:27 -0600

    classes black diamond attemted

--- classes.js
+++ classes.js
@@ -161,5 +161,17 @@ class ProgressiveManager extends Manager {
 */
 
 //Code Here
+class Machine {
+  constructor () {
+      this.widgets_made_count = 0;
+      this.wear_and_tear_count = 0;
+      this.needs_reboot = false;
+  }
+  makeWidgets (num) {
+    this.widgets_made_count += num;
+    this.wear_and_tear_count
+  }
+}
+
 
 
